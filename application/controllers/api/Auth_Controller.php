9114bdfa311e7e90d83153845f90db06e080a6ff
Jkanpariy2004 <jenish.rising@gmail.com>
2025-03-05 16:58:35 +0530

    JWT Token Apply

--- application/controllers/api/Auth_Controller.php
+++ application/controllers/api/Auth_Controller.php
@@ -1,20 +1,23 @@
 <?php
 
+use \Firebase\JWT\JWT;
+// composer require firebase/php-jwt
+
 class Auth_Controller extends RestApi_Controller
 {
-	function __construct()
+	public function __construct()
 	{
 		parent::__construct();
 		$this->load->library('api_auth');
 		$this->load->model('api_model');
 	}
 
-	function register()
+	public function register()
 	{
 		$fullname = $this->input->post('fullname');
-		$email = $this->input->post('email');
+		$email    = $this->input->post('email');
 		$phone_no = $this->input->post('phone_no');
-		$dob = $this->input->post('dob');
+		$dob      = $this->input->post('dob');
 		$password = $this->input->post('password');
 
 		$this->form_validation->set_rules('fullname', 'fullname', 'required');
@@ -24,46 +27,46 @@ class Auth_Controller extends RestApi_Controller
 		$this->form_validation->set_rules('password', 'password', 'required');
 		$this->form_validation->set_rules('confirm_password', 'confirm_password', 'required|matches[password]');
 
-		if ($this->form_validation->run() == FALSE) {
-			$responseData = array(
-				'status' => true,
+		if ($this->form_validation->run() == false) {
+			$responseData = [
+				'status'  => true,
 				'message' => 'please fix the validation errors.',
-				'errors' => $this->form_validation->error_array()
-			);
+				'errors'  => $this->form_validation->error_array(),
+			];
 			return $this->response($responseData);
 		} else {
-			$data = array(
+			$data = [
 				'fullname' => $fullname,
-				'email' => $email,
+				'email'    => $email,
 				'phone_no' => $phone_no,
-				'dob' => $dob,
-				'password' => password_hash($password, PASSWORD_DEFAULT)
-			);
+				'dob'      => $dob,
+				'password' => password_hash($password, PASSWORD_DEFAULT),
+			];
 
 			$this->api_model->registerUser($data);
-			$responseData = array(
-				'status' => true,
+			$responseData = [
+				'status'  => true,
 				'message' => 'User registered successfully',
-				'data' => $data
-			);
+				'data'    => $data,
+			];
 			return $this->response($responseData, 200);
 		}
 	}
 
-	function login()
+	public function login()
 	{
-		$email = $this->input->post('email');
+		$email    = $this->input->post('email');
 		$password = $this->input->post('password');
 
 		$this->form_validation->set_rules('email', 'email', 'required|valid_email');
 		$this->form_validation->set_rules('password', 'password', 'required');
 
-		if ($this->form_validation->run() == FALSE) {
-			$responseData = array(
-				'status' => true,
-				'message' => 'please fix the validation errors.',
-				'errors' => $this->form_validation->error_array()
-			);
+		if ($this->form_validation->run() == false) {
+			$responseData = [
+				'status'  => true,
+				'message' => 'Please fix the validation errors.',
+				'errors'  => $this->form_validation->error_array(),
+			];
 			return $this->response($responseData);
 		} else {
 			$user = $this->api_model->getUserByEmail($email);
@@ -72,26 +75,36 @@ class Auth_Controller extends RestApi_Controller
 			} else {
 				$loginStatus = false;
 			}
-			if($loginStatus != false){
-				$userId = $loginStatus->id;
-				$token = bin2hex(random_bytes(32));
-				$expiry = date('Y-m-d H:i:s', strtotime('+1 hour'));
-				$this->api_model->storeToken($userId, $token, $expiry);
+			if ($loginStatus != false) {
+				$userId    = $loginStatus->id;
+				$tokenData = [
+					'id' => $user->id,
+					"fullname" => $user->fullname,
+					"email" => $user->email,
+					"phone_no" => $user->phone_no,
+					"dob" => $user->dob,
+					"password" => $user->password,
+					'exp' => time() + 3600,
+				];
+				$secretKey = bin2hex(random_bytes(64));
 
-				$responseData = array(
-					'status' => true,
-					'message' => 'Login successful',
-					'data' => $loginStatus,
-					'token' => $token,
-					'token_expiry' => $expiry
-				);
+				$token = JWT::encode($tokenData, $secretKey, 'HS256');
+
+				$this->api_model->storeToken($userId, $token, date('Y-m-d H:i:s', strtotime('+1 hour')));
+
+				$responseData = [
+					'status'       => true,
+					'message'      => 'Login successful',
+					'token'        => $token,
+					'token_expiry' => date('Y-m-d H:i:s', strtotime('+1 hour')),
+					'data'         => $loginStatus,
+				];
 				return $this->response($responseData, 200);
-			}
-			else {
-				$responseData = array(
-					'status' => false,
-					'message' => 'Invalid login credentials'
-				);
+			} else {
+				$responseData = [
+					'status'  => false,
+					'message' => 'Invalid login credentials',
+				];
 				return $this->response($responseData, 401);
 			}
 		}
